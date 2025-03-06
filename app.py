d29ffc0272a583e68a0618c5efe6f28d35149481
Alicia <alicia.lim67@gmail.com>
2025-03-06 17:56:59 +0100

    augmented prompt + chatgpt

--- app.py
+++ app.py
@@ -2,37 +2,89 @@
 from pinecone import Pinecone
 from transformers import BertTokenizer, BertModel
 from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
+import os
 
-# pc = Pinecone(api_key="PINECONE_API_KEY")
-pc = Pinecone(api_key="pcsk_JAgF7_A3pvgNDs3t1BBAey769QZCwuRLcDZ9dqFmgP4yHnZvWuRTRP7mnLxxy1aZ4qcFL")
+import streamlit as st
+import time
+
+from langchain.schema import (
+    SystemMessage,
+    HumanMessage,
+    AIMessage
+)
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY") or "YOUR_API_KEY" #
+os.environ["PINECONE_API_KEY"] = os.getenv("PINECONE_API_KEY") or "YOUR_API_KEY" #
+
+chat = ChatOpenAI(
+    openai_api_key=os.environ["OPENAI_API_KEY"],
+    model='gpt-3.5-turbo'
+)
 
 index_name = "llama-2"
+pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
 index = pc.Index(index_name) # connect to index
 print("Connected to index")
 
-# load Bert model
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-model = BertModel.from_pretrained("bert-base-uncased")
-print("loading model")
+def get_augmented_prompt(query):
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    model = BertModel.from_pretrained("bert-base-uncased")
 
-def embedding_function(query):
     encoded_input = tokenizer(query, return_tensors='pt')
     embeds = model(**encoded_input).pooler_output[0]
-    return embeds
 
-text_field = "text"  # the metadata field that contains our text
-
-query = "What is so special about Llama 2?"
-embed = embedding_function(query).tolist()
-print(len(embed))
-
-s = index.query(
+    s = index.query(
     namespace="",
-    vector=embed,
+    vector=embeds,
     top_k=1,
     include_values=False,
     include_metadata=True,
-)
-print(s)
-text = s['matches'][0]['metadata']['text']
-print(text)
+    )
+    context = s['matches'][0]['metadata']['text']
+
+    augmented_prompt = f"""Using the contexts below, answer the query.
+
+    Contexts:
+    {context}
+
+    Query: {query}"""
+
+    return augmented_prompt
+
+def generate_response(chat, messages):
+    
+    response = chat(messages)
+    print("response", response.content)
+
+    for word in response.split():
+        print("word", word)
+        yield word + " "
+        time.sleep(0.5)
+
+st.title("RAG - Chatbot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    messages = [SystemMessage(content="You are a helpful assistant.")]
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("How can I help you?"):
+    # Display user message in chat message container
+    augmented_prompt= get_augmented_prompt(prompt)
+    st.chat_message("user").markdown(augmented_prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": augmented_prompt})
+    messages.append(HumanMessage(content=augmented_prompt))
+    
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        response = st.write_stream(generate_response(chat, messages))
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
+    messages.append(AIMessage(content=response))
